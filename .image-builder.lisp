a47b349c7d278e3b53e3406f3280c1854a9f9a6b
Sébastien Gross <sebchezwamorg>
2017-02-16 22:42:50 +0100

    Add image builder to dependencies
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- .image-builder.lisp
+++ .image-builder.lisp
@@ -2,7 +2,8 @@
 (:custom-systems
  (;;(:url "https://github.com/mabragor/esrap-liquid.git" :method :git)
   ;;(:url "https://github.com/mabragor/cl-yaclyaml.git" :method :git)
-  (:url "https://github.com/renard/cl-cli.git" :method :git))
+  (:url "https://github.com/renard/cl-cli.git" :method :git)
+  (:url "https://github.com/renard/cl-image-builder.git" :method :git))
  :packages (:ssh-config)
  :entry-point "ssh-config:main"
  :output-file "update-ssh-config")
